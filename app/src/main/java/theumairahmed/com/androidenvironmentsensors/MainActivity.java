b8f7af49b054ed469c500f95c236a54ae7f6f30b
Marry <theumairahmed@gmail.com>
2016-09-12 17:49:07 +0500

    Nice piece of fish

--- app/src/main/java/theumairahmed/com/androidenvironmentsensors/MainActivity.java
+++ app/src/main/java/theumairahmed/com/androidenvironmentsensors/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Fragment;
 /**
  * Created by Umair Ahmed on 9/3/2016.
  */
+
+//Nice piece of Fish :-)
 public class MainActivity extends SingleFragmentActivity {
     @Override
     public Fragment createFragment() {
